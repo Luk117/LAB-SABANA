2b05b0c6b9e93357af769db766728feb1c6b5dcc
Luk <lukasnieto117@gmail.com>
2021-02-20 21:28:53 -0500

    actualizacion semana 1

---
+++ SABANA-POOB-Robot-1.0.0-ClasesObyCons/Block.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Block here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Block extends Actor
+{
+    /**
+     * Act - do whatever the Block wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int speed;
+    public void act() 
+    {
+     turn(2);
+     
+        
+    }    
+}
